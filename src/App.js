39047160cdf4c0983c85932501fddf84eaa6c75f
Harshit Anand <harshitanand97@gmail.com>
2022-02-28 14:27:58 +0530

    Feat: New components added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Header from "./components/Header";
+import AddressTile from "./components/AddressTile";
+import AddressForm from "./components/AddressForm";
 
 function App() {
   return (
     <div className="App">
-        <h1>Hello</h1>
+        <h1>Address Management</h1>
+        <Header/>
+        <AddressTile/>
+        <AddressForm/>
     </div>
   );
 }
