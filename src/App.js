4d5a8eb583f82d15890f538f188107f63d1c6261
Harshit Anand <harshitanand97@gmail.com>
2022-02-28 18:10:21 +0530

    Feat: Form toggle functionality added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import './App.css';
 import Header from "./components/Header";
 import AddressTile from "./components/AddressTile";
 import AddressForm from "./components/AddressForm";
+import { useState } from 'react';
 
 function App() {
+
+  const [visible, setVisible] = useState(true);
+
+  function toggleAddressForm (){
+    setVisible(()=>!visible);
+  }
+
   return (
     <div className="App">
         <h1>Address Management</h1>
         <Header/>
-        <AddressTile/>
-        <AddressForm/>
+        {visible && <AddressTile toggle={toggleAddressForm}/>}
+        {!visible && <AddressForm/>}
     </div>
   );
 }
--- src/components/AddressTile.js
+++ src/components/AddressTile.js
@@ -3,9 +3,9 @@ import AddIcon from '@mui/icons-material/Add';
 import "../App.css";
 import "../css/AddressTile.css"
 
-function AddressTile() {
+function AddressTile({toggle}) {
   return (
-    <div className='address-tile'>
+    <div className='address-tile' onClick={toggle}>
         <div className="address-tile__icon">
           <h3>Add Address</h3>
           <AddIcon sx={{color:"#B0B0B0",width:"3rem", height:"3rem"}}/>
