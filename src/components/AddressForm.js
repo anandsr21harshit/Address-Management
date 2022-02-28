fc8614c9efb39aeefd61869680b3551499c80a14
Harshit Anand <harshitanand97@gmail.com>
2022-02-28 18:10:53 +0530

    Placeholder and name added in input tags

--- src/components/AddressForm.js
+++ src/components/AddressForm.js
@@ -6,39 +6,37 @@ function AddressForm() {
     <form className='form-container'>
         <div className="form-container__label-input">
             <label htmlFor="">Name</label>
-            <input type="text" />
+            <input type="text" placeholder='Name' name='name' />
         </div>
 
         <div className="form-container__label-input">
             <label htmlFor="">Mobile</label>
-            <input type="number" name="" id="" />
+            <input type="number" placeholder='Mobile' name='mobile'/>
         </div>
         
         <div className="form-container__label-input">
             <label htmlFor="">Address Line 1</label>
-            <input type="text" />
+            <input type="text" placeholder='Address Line 1' name='address1' />
         </div>
         
         <div className="form-container__label-input">
             <label htmlFor="">Address Line 2</label>
-            <input type="text" />
+            <input type="text" placeholder='Address Line 2' name='address2' />
         </div>
+
         <div className="form-container__label-input">
-            <label htmlFor="">Address Line 2</label>
-            <input type="text" />
-        </div>
-        <div className="form-container__label-input">
-            <label htmlFor="">Address Line 3</label>
-            <input type="text" />
+            <label htmlFor="">City</label>
+            <input type="text" placeholder='City' name='city' />
         </div>
         <div className="form-container__label-input">
             <label htmlFor="">State</label>
-            <input type="text" />
+            <input type="text" placeholder='State'  name='state'/>
         </div>
         <div className="form-container__label-input">
             <label htmlFor="">Pincode</label>
-            <input type="number" name="" id="" />
+            <input type="number" placeholder='Pincode'  name='pincode'/>
         </div>
+        <button type="submit">Add Address</button>
     </form>
   )
 }
