37cda7c3dfb3879ad025dadb8d71494edda7366a
Harshit Anand <harshitanand97@gmail.com>
2022-03-01 14:16:38 +0530

    Fix: non submission of form

--- src/components/AddressForm.js
+++ src/components/AddressForm.js
@@ -4,20 +4,30 @@ import axios from "axios";
 
 const url = "https://621c6704768a4e1020a909e2.mockapi.io/users"
 
-function AddressForm() {
-
+function AddressForm({visible, setVisible}) {
+    
+    // object to store data from input tag and then post to server
     const formDataObj = {};
 
     const [formData, setFormData] = useState(formDataObj);
 
+    //handler for input tags
     function dataChangeHandler(e){
         setFormData((formData)=> ({...formData,[e.target.name]:e.target.value}));
     }
-    console.log(formData);
 
+
+    //submitting data to server
     function formSubmitHandler(e){
+        e.preventDefault();
         const response = axios.post(url,formData);
-        console.log(response);
+        setVisible(true);
+        console.log(response.data);
+    }
+
+    //closing form
+    function cancelHandler(){
+        setVisible(true);
     }
 
   return (
@@ -55,6 +65,7 @@ function AddressForm() {
             <input type="number" placeholder='Pincode'  name='pincode' onChange={dataChangeHandler}/>
         </div>
         <button type="submit">Add Address</button>
+        <button onClick={cancelHandler}>Cancel</button>
     </form>
   )
 }
