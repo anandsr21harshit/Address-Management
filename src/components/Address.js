13efd3d230fe7d1bf5cf1d6abb8b0e81391abe83
Harshit Anand <harshitanand97@gmail.com>
2022-03-03 19:50:05 +0530

    Feat: Added Edit button functionality

--- src/components/Address.js
+++ src/components/Address.js
@@ -1,18 +1,34 @@
 import React from 'react'
 import "../css/Address.css"
 
-function Address({people,removeItem}) {
+function Address({people,removeItem, setEditForm}) {
+
+  // setting data to local storage
+  const setData = (people)=>{
+    const {name,address1,address2,mobile,city,state,pincode,id} = people;
+    localStorage.setItem('name',name);
+    localStorage.setItem('address1',address1);
+    localStorage.setItem('address2',address2);
+    localStorage.setItem('mobile',mobile);
+    localStorage.setItem('city',city);
+    localStorage.setItem('state',state);
+    localStorage.setItem('pincode',pincode);
+    localStorage.setItem('id',id)
+  }
   return (
     <ul className='list-item'>
         <li><h3>{people.name}</h3></li>
         <li>{people.address1}</li>
         <li>{people.address2}</li>
-        <li>City: {people.city}</li>
         <li>Mobile: {people.mobile}</li>
+        <li>{people.city}</li>
         <li>{people.state}</li>
-        <li>ID: {people.id}</li>
+        <li>{people.pincode}</li>
         <div className="list-item__btn-container">
-            <button>Edit</button>
+            <button onClick={()=>{
+              setEditForm(true);
+              setData(people);
+              }}>Edit</button>
             <button onClick={()=>removeItem(people.id)}>Remove</button>
         </div>
     </ul>
